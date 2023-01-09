45cc2527a76770db43f4ceaf094cf676c38c22e7
Vojtech Simetka <vojtech@simetka.cz>
2023-01-09 22:30:36 +0700

    fix: deployment script for the contract

--- packages/contracts/tasks/deploy.ts
+++ packages/contracts/tasks/deploy.ts
@@ -2,7 +2,7 @@ import { task, types } from "hardhat/config"
 
 task("deploy", "Deploy a GlobalAnonymousFeed contract")
     .addOptionalParam("semaphore", "Semaphore contract address", undefined, types.string)
-    .addOptionalParam("group", "Group id", "42", types.string)
+    .addOptionalParam("group", "Group id", 42, types.int)
     .addOptionalParam("logs", "Print the logs", true, types.boolean)
     .setAction(async ({ logs, semaphore: semaphoreAddress, group: groupId }, { ethers, run }) => {
         if (!semaphoreAddress) {
