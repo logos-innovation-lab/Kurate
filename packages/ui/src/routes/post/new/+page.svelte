9e332bbc15011935e258a6552dbebcc52c4e7c5a
Barbara Gomes <sugarpusher@users.noreply.github.com>
2023-01-31 00:29:16 -0500

    style: set max-width and padding to new post page header (#131)
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- packages/ui/src/routes/post/new/+page.svelte
+++ packages/ui/src/routes/post/new/+page.svelte
@@ -84,6 +84,14 @@
 		align-items: center;
 		font-weight: 600;
 		padding: var(--spacing-12) var(--spacing-12) var(--spacing-24);
+		margin: 0 auto;
+		transition: padding 0.2s;
+		max-width: var(--container-width);
+
+		@media (min-width: 1280px) {
+			padding-top: var(--spacing-48);
+			transition: padding 0.2s;
+		}
 	}
 
 	.btns {
@@ -97,5 +105,9 @@
 		padding: var(--spacing-24) var(--spacing-12) var(--spacing-12);
 		height: calc(100vh - 80px);
 		text-align: center;
+
+		@media (min-width: 1280px) {
+			height: calc(100vh - 116px);
+		}
 	}
 </style>
