b680164d1da7d5af4e1b055b1257078e8a971a2e
Vojtech Simetka <vojtech@simetka.cz>
2023-02-07 15:02:44 +0700

    chore: fix ui test

--- packages/ui/tests/test.ts
+++ packages/ui/tests/test.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test'
 
 test('index page has expected header', async ({ page }) => {
 	await page.goto(ROUTES.HOME)
-	expect(await page.textContent('span')).toBe('The Outlet')
+	expect(await page.textContent('span')).toBe('Kurate')
 })
