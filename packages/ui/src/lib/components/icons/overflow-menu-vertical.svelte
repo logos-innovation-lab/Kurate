3db6379a9bbea7959983bc8c286ec007d3944e8f
Barbara Gomes <sugarpusher@users.noreply.github.com>
2023-03-16 13:52:01 -0400

    style: add dropdown menus to chat headers (#260)
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

---
+++ packages/ui/src/lib/components/icons/overflow-menu-vertical.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { IconProps } from '$lib/types'
+
+	type $$Props = IconProps
+
+	let cls: string | undefined = undefined
+	export { cls as class }
+	export let size = 20
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width={size} height={size} class={cls}>
+	<circle cx="16" cy="8" r="2" />
+	<circle cx="16" cy="16" r="2" />
+	<circle cx="16" cy="24" r="2" />
+</svg>
+
