9c7f359aa5044abde099260ab3e30ea4122c11d6
Barbara Gomes <sugarpusher@users.noreply.github.com>
2023-03-18 18:50:48 -0400

    style: fix header on account/profile page (#263)
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- packages/ui/src/routes/profile/+page.svelte
+++ packages/ui/src/routes/profile/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { browser } from '$app/environment'
 	import Button from '$lib/components/button.svelte'
+	import Header from '$lib/components/header.svelte'
 	import Logout from '$lib/components/icons/logout.svelte'
-	import Undo from '$lib/components/icons/undo.svelte'
 	import Wallet from '$lib/components/icons/wallet.svelte'
 	import WalletInfo from '$lib/components/wallet-info.svelte'
 	import { formatAddress } from '$lib/utils/format'
@@ -24,16 +24,12 @@
 			error = err as Error
 		}
 	}
+
 </script>
 
 <svelte:window bind:scrollY={y} />
 
-<div class={`header ${y > 0 ? 'scrolled' : ''}`}>
-	<div>
-		<Button icon={Undo} on:click={() => history.back()} />
-		<h1>Account</h1>
-	</div>
-</div>
+<Header title="Account" onBack={() => history.back()} />
 <div class="content">
 	{#if $profile.signer === undefined}
 		<div class="wallet-icon-wrapper">
@@ -91,47 +87,6 @@
 		}
 	}
 
-	.header {
-		position: sticky;
-		top: 0;
-		left: 0;
-		right: 0;
-		padding: var(--spacing-12);
-		background-color: rgba(var(--color-body-bg-rgb), 0.93);
-		backdrop-filter: blur(3px);
-		transition: padding 0.2s;
-
-		@media (min-width: 1280px) {
-			border-bottom: none;
-			padding-top: var(--spacing-48);
-			transition: padding 0.2s;
-		}
-
-		&.scrolled {
-			box-shadow: 0 1px 5px 0 rgba(var(--color-body-text-rgb), 0.25);
-
-			@media (min-width: 1280px) {
-				padding-top: var(--spacing-12);
-				transition: padding 0.2s;
-			}
-		}
-
-		> div:first-child {
-			display: flex;
-			flex-direction: row;
-			align-items: center;
-			gap: var(--spacing-12);
-
-			@media (min-width: 1280px) {
-				width: 1280px;
-				margin: 0 auto;
-			}
-
-			h1 {
-				font-weight: 600;
-			}
-		}
-	}
 	.content {
 		display: flex;
 		flex-direction: column;
