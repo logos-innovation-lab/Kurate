f7bbd78bf895b9d7d727d6b19081b3d95e1e6a8f
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-04-02 15:04:44 -0400

    style: fix icon alignment in button

--- packages/ui/src/lib/components/button.svelte
+++ packages/ui/src/lib/components/button.svelte
@@ -58,9 +58,6 @@
 
 		.wrapper {
 			margin-right: 0px;
-			display: flex;
-			justify-content: center;
-			align-items: center;
 		}
 
 		&.small {
@@ -72,6 +69,9 @@
 		width: 20px;
 		height: 20px;
 		margin-right: var(--spacing-6);
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 	.primary {
 		color: var(--color-body-bg);
